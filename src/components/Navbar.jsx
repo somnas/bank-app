9e77329593f81de54e8f61b88f186c90b35dce5b
Erik Somnäs <erik.somnas@gmail.com>
2022-10-21 16:53:54 +0200

    mobile navbar

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -25,6 +25,17 @@ export default function Navbar() {
 					className='w-[28px] h-[28px] object-contain cursor-pointer'
 					onClick={() => setToggle((prev) => !prev)}
 				/>
+				<div className={`${toggle ? 'flex' : 'hidden'} p-6 bg-black-gradient absolute top-20 right-0 mx-4 my-2 min-w-[140px] rounded-xl sidebar`}>
+					<ul className='list-none flex flex-col justify-end items-center flex-1'>
+						{navLinks.map((link, index) => (
+							<li key={link.id} className={`font-poppins font-normal cursor-pointer text-[16px] ${index === navLinks.length - 1 ? 'mb-0' : 'mb-4'} text-white`}>
+								<a href={`#${link.id}`}>
+									{link.title}
+								</a>
+							</li>
+						))}
+					</ul>
+				</div>
 			</div>
 		</nav>
 	)
