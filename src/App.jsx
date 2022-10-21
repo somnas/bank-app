ddb1585172797978a0db6b738a9ad912850b6b14
Erik Somnäs <erik.somnas@gmail.com>
2022-10-21 15:47:56 +0200

    import styles file

--- src/App.jsx
+++ src/App.jsx
@@ -2,6 +2,10 @@ import React from 'react';
 
 export default function App() {
 	return (
-		<div>App</div>
+		<div className='bg-primary w-full overflow-hidden'>
+			<div>
+				
+			</div>
+		</div>
 	)
 }
