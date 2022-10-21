8e45c003fe5c735d003f03d66dce2e88964f3c05
Erik Somnäs <erik.somnas@gmail.com>
2022-10-21 16:30:49 +0200

    wip

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,7 +1,22 @@
-import React from 'react'
+import React, { useState } from 'react';
+
+import { close, logo, menu } from '../assets';
+import { navLinks } from '../constants';
 
 export default function Navbar() {
-  return (
-    <div>Navbar</div>
-  )
+	console.log(navLinks);
+	return (
+		<nav className='w-full flex py-6 justify-between items-center navbar'>
+			<img src={logo} alt='hoobank' className='w-[124px] h-[32px]' />
+			<ul className='list-none sm:flex hidden justify-end items-center flex-1'>
+				{navLinks.map((link, index) => (
+					<li key={link.id} className={`font-poppins`}>
+						<a href={`#${link.id}`}>
+							{link.title}
+						</a>
+					</li>
+				))}
+			</ul>
+		</nav>
+	)
 }
