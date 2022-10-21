8ff99a9e83c430e3bf3fd46f20884dd7d1e154c7
Erik Somnäs <erik.somnas@gmail.com>
2022-10-21 16:41:24 +0200

    add mobile menu

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -4,19 +4,28 @@ import { close, logo, menu } from '../assets';
 import { navLinks } from '../constants';
 
 export default function Navbar() {
-	console.log(navLinks);
+
+	const [toggle, setToggle] = useState(false);
+
 	return (
 		<nav className='w-full flex py-6 justify-between items-center navbar'>
 			<img src={logo} alt='hoobank' className='w-[124px] h-[32px]' />
 			<ul className='list-none sm:flex hidden justify-end items-center flex-1'>
 				{navLinks.map((link, index) => (
-					<li key={link.id} className={`font-poppins`}>
+					<li key={link.id} className={`font-poppins font-normal cursor-pointer text-[16px] ${index === navLinks.length - 1 ? 'mr-0' : 'mr-10'} text-white`}>
 						<a href={`#${link.id}`}>
 							{link.title}
 						</a>
 					</li>
 				))}
 			</ul>
+			<div className='sm:hidden flex flex-1 justify-end items-center'>
+				<img src={toggle ? close : menu}
+					alt='menu'
+					className='w-[28px] h-[28px] object-contain cursor-pointer'
+					onClick={() => setToggle((prev) => !prev)}
+				/>
+			</div>
 		</nav>
 	)
 }
