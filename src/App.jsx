ce1a5f486b39f9c2c5b49716059de6d87d8ee77c
Erik Somnäs <erik.somnas@gmail.com>
2022-10-21 16:14:32 +0200

    import components

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,7 @@
 import React from 'react';
 import styles from './style';
 
-import Navbar from './components/Navbar';
+import { Billing, Business, CardDeal, Clients, CTA, Footer, Navbar, Stats, Testimonials, Hero } from "./components";
 
 export default function App() {
 	return (
