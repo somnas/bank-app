a5f7c32f58b099c2c08af62a08e5ecc01174e907
Erik Somnäs <erik.somnas@gmail.com>
2022-10-21 16:04:46 +0200

    wip

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,14 @@
 import React from 'react';
+import styles from './style'
 
 export default function App() {
 	return (
 		<div className='bg-primary w-full overflow-hidden'>
-			<div>
-				
-			</div>
+			{/* <div className={`${styles.paddingX} ${styles.flexCenter}`}>
+				<div className={`${styles.boxWidth}`}> */}
+					<h1>Navbar</h1>
+				{/* </div>
+			</div> */}
 		</div>
 	)
 }
