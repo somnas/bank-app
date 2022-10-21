bb85407fe1f0f6b2b9e2446f9c0e972a8629a16d
Erik Somnäs <erik.somnas@gmail.com>
2022-10-21 16:13:11 +0200

    create components

--- src/App.jsx
+++ src/App.jsx
@@ -1,14 +1,35 @@
 import React from 'react';
-import styles from './style'
+import styles from './style';
+
+import Navbar from './components/Navbar';
 
 export default function App() {
 	return (
 		<div className='bg-primary w-full overflow-hidden'>
-			{/* <div className={`${styles.paddingX} ${styles.flexCenter}`}>
-				<div className={`${styles.boxWidth}`}> */}
-					<h1>Navbar</h1>
-				{/* </div>
-			</div> */}
+			<div className={`${styles.paddingX} ${styles.flexCenter}`}>
+				<div className={`${styles.boxWidth}`}>
+					Navbar
+				</div>
+			</div>
+
+			<div className={`bg-primary ${styles.flexStart}`}>
+				<div className={`${styles.boxWidth}`}>
+					Hero
+				</div>
+			</div>
+
+			<div className={`bg-primary ${styles.paddingX} ${styles.flexStart}`}>
+				<div className={`${styles.boxWidth}`}>
+					Stats
+					Business
+					Billing
+					CardDeal
+					Testimonials
+					Clients
+					CTA
+					Footer
+				</div>
+			</div>
 		</div>
 	)
 }
